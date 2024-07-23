bedd8b917a5b233bad2f5575efab8c4742e2d2f2
Margaux <margaux.espinasse@gmail.com>
2024-07-23 11:04:17 +0200

    Update Scripts.js with pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert("Hello world");
-let favoriteFood = "Tacos";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Pidgey",
+    height: 0.3,
+    types: ["flying", "normal"],
+  },
+  {
+    name: "Rapidash",
+    height: 1.7,
+    types: ["fire"],
+  },
+];
