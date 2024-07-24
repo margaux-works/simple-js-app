ad7d2ab9e9cfea82ca8e3ab494375f30ac1b71c5
Margaux <margaux.espinasse@gmail.com>
2024-07-24 14:01:55 +0200

    Home content added, first JS loop added

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,19 @@ let pokemonList = [
     types: ['fire'],
   },
 ];
+
+// list all Pokemon name and their height, incl. a message about height
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemonName = pokemonList[i].name;
+  let pokemonHeight = pokemonList[i].height;
+
+  let message = '';
+  if (pokemonHeight >= 1.2) {
+    message = "- Wow, that's big!";
+  } else {
+    message = '- Small, but deadly.';
+  }
+
+  document.write(`${pokemonName} (height:${pokemonHeight}) ${message}<br>`);
+}
